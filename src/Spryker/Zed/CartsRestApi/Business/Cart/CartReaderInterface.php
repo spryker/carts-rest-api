cadfdc7f07ce67d737fc0b111fd101d177904716
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2018-11-14 22:19:12 +0000

    GLUE-391 WIP Quote retrival moved to zed [skip ci]

--- src/Spryker/Zed/CartsRestApi/Business/Cart/CartReaderInterface.php
+++ src/Spryker/Zed/CartsRestApi/Business/Cart/CartReaderInterface.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Client\CartsRestApi\Reader;
+namespace Spryker\Zed\CartsRestApi\Business\Cart;
 
 use Generated\Shared\Transfer\QuoteCollectionTransfer;
 use Generated\Shared\Transfer\QuoteCriteriaFilterTransfer;
--- src/Spryker/Zed/CartsRestApi/Business/Quote/SingleQuoteCollectionReaderInterface.php
+++ src/Spryker/Zed/CartsRestApi/Business/Quote/SingleQuoteCollectionReaderInterface.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Client\CartsRestApi\Reader;
+namespace Spryker\Zed\CartsRestApi\Business\Quote;
 
 use Generated\Shared\Transfer\QuoteCollectionTransfer;
 use Generated\Shared\Transfer\QuoteCriteriaFilterTransfer;
