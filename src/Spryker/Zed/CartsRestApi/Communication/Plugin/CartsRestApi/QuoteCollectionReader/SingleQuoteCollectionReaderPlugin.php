e072afbf4bc61bcd360b9f6b79bed5aebdf38b52
voitov <tigran.voitov@spryker.com>
2019-01-29 19:41:42 +0200

    glue-2279 glue-2285 fixes, toDos to reduce qty of zed calls

---
+++ src/Spryker/Zed/CartsRestApi/Communication/Plugin/CartsRestApi/QuoteCollectionReader/SingleQuoteCollectionReaderPlugin.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\CartsRestApi\Communication\Plugin\CartsRestApi\QuoteCollectionReader;
+
+use Generated\Shared\Transfer\QuoteCollectionResponseTransfer;
+use Generated\Shared\Transfer\RestQuoteCollectionRequestTransfer;
+use Spryker\Zed\CartsRestApiExtension\Dependency\Plugin\QuoteCollectionReaderPluginInterface;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+/**
+ * @method \Spryker\Zed\CartsRestApi\Business\CartsRestApiFacadeInterface getFacade()
+ * @method \Spryker\Zed\CartsRestApi\CartsRestApiConfig getConfig()
+ * @method \Spryker\Zed\CartsRestApi\Communication\CartsRestApiCommunicationFactory getFactory()
+ */
+class SingleQuoteCollectionReaderPlugin extends AbstractPlugin implements QuoteCollectionReaderPluginInterface
+{
+    /**
+     * {@inheritdoc}
+     * - Returns quote collection transfer with list of quotes for customer.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\RestQuoteCollectionRequestTransfer $restQuoteCollectionRequestTransfer
+     *
+     * @return \Generated\Shared\Transfer\QuoteCollectionResponseTransfer
+     */
+    public function getQuoteCollection(
+        RestQuoteCollectionRequestTransfer $restQuoteCollectionRequestTransfer
+    ): QuoteCollectionResponseTransfer {
+        return $this->getFacade()->findQuoteByCustomerAndStore($restQuoteCollectionRequestTransfer);
+    }
+}
