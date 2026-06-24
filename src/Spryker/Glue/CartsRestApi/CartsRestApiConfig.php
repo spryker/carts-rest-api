b3f7938a7b7cfe8dfbaede56f29c3a5c1c6521e5
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/CartsRestApi/CartsRestApiConfig.php
+++ src/Spryker/Glue/CartsRestApi/CartsRestApiConfig.php
@@ -15,16 +15,22 @@ use Symfony\Component\HttpFoundation\Response;
 class CartsRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CARTS = 'carts';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CART_ITEMS = 'items';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\CustomersRestApi\CustomersRestApiConfig::RESOURCE_CUSTOMERS
      *
      * @var string
@@ -32,226 +38,316 @@ class CartsRestApiConfig extends AbstractBundleConfig
     public const RESOURCE_CUSTOMERS = 'customers';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_GUEST_CARTS = 'guest-carts';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_GUEST_CARTS_ITEMS = 'guest-cart-items';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_CARTS = 'carts-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_CART_ITEMS = 'cart-items-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_GUEST_CARTS = 'guest-carts-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_GUEST_CART_ITEMS = 'guest-cart-items-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CART_NOT_FOUND = '101';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_ITEM_VALIDATION = '102';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_ITEM_NOT_FOUND = '103';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CART_ID_MISSING = '104';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_FAILED_DELETING_CART = '105';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_FAILED_DELETING_CART_ITEM = '106';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_FAILED_CREATING_CART = '107';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_ANONYMOUS_CUSTOMER_UNIQUE_ID_EMPTY = '109';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_ALREADY_HAS_CART = '110';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CART_CANT_BE_UPDATED = '111';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_STORE_DATA_IS_INVALID = '112';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_FAILED_ADDING_CART_ITEM = '113';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_FAILED_UPDATING_CART_ITEM = '114';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_UNAUTHORIZED_CART_ACTION = '115';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CURRENCY_DATA_IS_MISSING = '116';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CURRENCY_DATA_IS_INCORRECT = '117';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_PRICE_MODE_DATA_IS_MISSING = '118';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_PRICE_MODE_DATA_IS_INCORRECT = '119';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_UNAUTHORIZED = '802';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_CART_ID_MISSING = 'Cart uuid is missing.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_CART_ITEM_NOT_FOUND = 'Item with the given group key not found in the cart.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_FAILED_TO_CREATE_CART = 'Failed to create cart.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_CART_WITH_ID_NOT_FOUND = 'Cart with given uuid not found.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_FAILED_DELETING_CART = 'Cart could not be deleted.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_FAILED_DELETING_CART_ITEM = 'Cart item could not be deleted.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_ANONYMOUS_CUSTOMER_UNIQUE_ID_EMPTY = 'Anonymous customer unique id is empty.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_CUSTOMER_ALREADY_HAS_CART = 'Customer already has a cart.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_PRICE_MODE_CANT_BE_CHANGED = 'Can’t switch price mode when there are items in the cart.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_STORE_DATA_IS_INVALID = 'Store data is invalid.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_FAILED_ADDING_CART_ITEM = 'Cart item could not be added.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_FAILED_UPDATING_CART_ITEM = 'Cart item could not be updated.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_UNAUTHORIZED_CART_ACTION = 'Unauthorized cart action.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_CURRENCY_DATA_IS_MISSING = 'Currency is missing.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_CURRENCY_DATA_IS_INCORRECT = 'Currency is incorrect.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_PRICE_MODE_DATA_IS_MISSING = 'Price mode is missing.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const EXCEPTION_MESSAGE_PRICE_MODE_DATA_IS_INCORRECT = 'Price mode is incorrect.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CUSTOMER_UNAUTHORIZED = 'Unauthorized request.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID = 'X-Anonymous-Customer-Unique-Id';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const HEADER_AUTHORIZATION = 'authorization';
--- src/Spryker/Shared/CartsRestApi/CartsRestApiConfig.php
+++ src/Spryker/Shared/CartsRestApi/CartsRestApiConfig.php
@@ -12,91 +12,127 @@ use Spryker\Shared\Kernel\AbstractBundleConfig;
 class CartsRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_CART_NOT_FOUND = 'ERROR_IDENTIFIER_CART_NOT_FOUND';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_CART_CANT_BE_UPDATED = 'ERROR_IDENTIFIER_CART_CANT_BE_UPDATED';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_ITEM_NOT_FOUND = 'ERROR_IDENTIFIER_ITEM_NOT_FOUND';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_FAILED_DELETING_CART = 'ERROR_IDENTIFIER_FAILED_DELETING_CART';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_FAILED_DELETING_CART_ITEM = 'ERROR_IDENTIFIER_FAILED_DELETING_CART_ITEM';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_FAILED_ADDING_CART_ITEM = 'ERROR_IDENTIFIER_FAILED_ADDING_CART_ITEM';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_FAILED_UPDATING_CART_ITEM = 'ERROR_IDENTIFIER_FAILED_UPDATING_CART_ITEM';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_FAILED_CREATING_CART = 'ERROR_IDENTIFIER_FAILED_CREATING_CART';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_ANONYMOUS_CUSTOMER_UNIQUE_ID_EMPTY = 'ERROR_IDENTIFIER_ANONYMOUS_CUSTOMER_UNIQUE_ID_EMPTY';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_CUSTOMER_ALREADY_HAS_CART = 'ERROR_IDENTIFIER_CUSTOMER_ALREADY_HAS_CART';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_STORE_DATA_IS_INVALID = 'ERROR_IDENTIFIER_STORE_DATA_IS_INVALID';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_UNAUTHORIZED_CART_ACTION = 'ERROR_IDENTIFIER_UNAUTHORIZED_CART_ACTION';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_QUOTE_NOT_AVAILABLE = 'ERROR_IDENTIFIER_QUOTE_NOT_AVAILABLE';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_PERMISSION_FAILED = 'ERROR_IDENTIFIER_PERMISSION_FAILED';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_CURRENCY_DATA_IS_MISSING = 'ERROR_IDENTIFIER_CURRENCY_DATA_IS_MISSING';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_CURRENCY_DATA_IS_INCORRECT = 'ERROR_IDENTIFIER_CURRENCY_DATA_IS_INCORRECT';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_PRICE_MODE_DATA_IS_MISSING = 'ERROR_IDENTIFIER_PRICE_MODE_DATA_IS_MISSING';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_IDENTIFIER_PRICE_MODE_DATA_IS_INCORRECT = 'ERROR_IDENTIFIER_PRICE_MODE_DATA_IS_INCORRECT';
